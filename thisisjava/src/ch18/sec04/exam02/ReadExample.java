4d521065304cfa85ad096acdba26b72cbe86cf08
KIMBS <iimkbsu@naver.com>
2023-06-01 21:22:57 +0900

    ThisIsJava.23.06.01.v01

--- thisisjava/src/ch18/sec04/exam02/ReadExample.java
+++ thisisjava/src/ch18/sec04/exam02/ReadExample.java
@@ -1,8 +1,8 @@
 package ch18.sec04.exam02;
 
 import java.io.Reader;
-import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadExample {
@@ -13,35 +13,65 @@ public class ReadExample {
 		
 		try {
 			reader = new FileReader("C:/ThisIsJavaTemp/test.txt");
-			//문자 하나씩 읽기
+			
+			int a = reader.read();
+			char aa = (char)reader.read();
+			
+			System.out.println("읽기스트림.read() : " + a + "\n(char) 읽기스트림.read() : " + aa);
+			System.out.println();
+			
+			
+			
+			char[] word = new char[1000];
+			
+			int length = reader.read(word);
+			
+			for(int i=0; i<length; i++) {
+				char data = word[i];
+				System.out.print(data);
+			}
+			
+			reader.close();
+			
+			System.out.println();
+			
+			reader = new FileReader("C:/ThisIsJavaTemp/test.txt");
 			while(true) {
 				int data = reader.read();
 				if(data == -1) {
 					break;
-				}else System.out.print((char)data);
-			}
-			reader.close();
-			System.out.println();
-			
-			//문자 배열로 읽기
-			reader = new FileReader("C:/ThisIsJavaTemp/test.txt");
-			char[] data = new char[1000];
-			
-			while(true) {
-				int num = reader.read(data);
-				if(num == -1) {
-					break;
 				}else {
-					for(int i=0; i<num; i++) {
-						System.out.print(data[i]);
-					}
+					System.out.print((char)data);
 				}
 			}
 			reader.close();
 			
-		}catch(IOException e) {
+			System.out.println();
+			
+			
+			reader = new FileReader("C:/ThisIsJavaTemp/test.txt");
+			
+			char[] data = new char[1000];
+			
+			while(true) {
+				int num = reader.read(data);
+				if(num == -1) break;
+				for(int i=0; i<num; i++) {
+					System.out.print(data[i]);
+				}
+			}
+			reader.close();
+		}
+		
+		catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("\n입력스트림 완료");
+		}
+		
 		
 
 	}
