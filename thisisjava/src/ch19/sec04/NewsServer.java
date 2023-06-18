adc85ded2fa58e448ac9d6a82fadcfdd7b9141f0
KIMBS <iimkbsu@naver.com>
2023-06-18 18:52:36 +0900

    ThisIsJava.23.06.18.v01

---
+++ thisisjava/src/ch19/sec04/NewsServer.java
@@ -0,0 +1,19 @@
+package ch19.sec04;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketAddress;
+
+import java.util.Scanner;
+
+public class NewsServer extends Thread {
+	
+	private static DatagramSocket dgs;
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
